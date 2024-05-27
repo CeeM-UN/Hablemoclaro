6231c50099bdf56792f08cc6e1d6f4f6249316cd
CeeM-UN <cepia@unal.edu.co>
2024-05-26 22:32:29 -0500

    home updated

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import './Home.css';
 import logo from '../Images/imagenlogo.png'; // Importa la imagen del logo aquí
-
+import background from '../Images/17ib2g.png'
+import imagen from '../Images/fondo-home.jpg';
 
 function Home() {
   return (
@@ -10,8 +11,11 @@ function Home() {
         <img src={logo} alt="Imagen del blog" className="blog-image" />
         <div className="centered-text">Hablemos Claro</div>
       </div>
+
+      <div className="cuerpo-blog" style={{ backgroundImage: `url(${background})` }}>
       <div className="text-container">Las emociones son los colores que pintan el lienzo de nuestra experiencia humana. Desde la euforia de la felicidad hasta la melancolía de la tristeza, cada emoción teje una narrativa única en el tapiz de nuestras vidas. En este viaje intrincado a través del laberinto de las emociones, exploraremos las profundidades de nuestro mundo interno y descubriremos la riqueza que yace dentro de cada sentimiento.</div> {/* Nuevo elemento */}
-      <div className="image-container">
+
+      <img src={imagen} alt="Imagen del blog" className="blog-image" />
     </div>
   </div>
   );
