188f704b4f47a44719fb4ed9285eafbb1d2f1d62
Alejonavas24 <alejonavas2442@gmail.com>
2024-05-26 22:33:18 -0500

    tabla insertando

--- src/pages/ReconociendoEmociones.js
+++ src/pages/ReconociendoEmociones.js
@@ -11,12 +11,24 @@ function Home() {
         <img src={logo} alt="Imagen reconociendo emociones" className="blog-image" />
         <div className="centered-text">Reconociendo Emociones </div>
       </div>
+  
     <div className="cuerpo-blog" style={{ backgroundImage: `url(${background})` }}>
       <div className="text-container">Sabemos que en la adolescencia se experimentan cambios físicos, cognitivos y emocionales significativos. Reconocer y comprender tus propias emociones, así como las de los demás, es crucial para su desarrollo emocional saludable.</div> {/* Nuevo elemento */}
     <div className="text-container">Te dejamos varios tips que te pueden ayudar, no solo en el entorno escolar sino que en cualquier entorno.</div>
-    <div><img scr={flecha_fondo} alt="imagen flecha" className="blog-image" /></div>
+    <img src={flecha_fondo} alt="flecha" className="flecha" />
+    <table border="1">
+     <tr>
+      <td>hola</td>
+      <td>hola 2</td>
+     </tr>
+     <tr>
+      <td>hola 3</td>
+      <td>hola 4</td>
+     </tr>
+    </table>
     </div>
     </div>
+    
   );
 }
 
