419cb31967b139369fb9b43cf0f7f8056fa9d0f6
CeeM-UN <cepia@unal.edu.co>
2024-05-26 20:45:34 -0500

    Primer commit con todo el proyecto

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import './App.css';
+
+// Importando tus componentes de página
+import Home from './pages/Home';
+import NuestroEscenario from './pages/NuestroEscenario';
+import ReconociendoEmociones from './pages/ReconociendoEmociones';
+import RinconOcio from './pages/RinconOcio';
+
+
+// Importando los estilos
+import './pages/Home.css';
+import './pages/NuestroEscenario.css';
+import './pages/ReconociendoEmociones.css';
+import './pages/RinconOcio.css';
+
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <h2>Hablemos Claro</h2>
+
+
+        <nav>
+          <ul style={{ display: 'flex', gap: '10px' }}>
+            <li><Link to="/">inicio</Link></li>
+            <li><Link to="/nuestro-escenario">Nuestro Escenario</Link></li>
+            <li><Link to="/reconociendo-emociones">Reconociendo Emociones</Link></li>
+            <li><Link to="/rincon-ocio">Rincón de Ocio</Link></li>
+          </ul>
+        </nav>
+      
+    </header>
+
+    {/* Aquí es donde se renderizarán tus componentes de página */}
+    <Route path="/" exact component={Home} />
+    <Route path="/nuestro-escenario" component={NuestroEscenario} />
+    <Route path="/reconociendo-emociones" component={ReconociendoEmociones} />
+    <Route path="/rincon-ocio" component={RinconOcio} />
+  </div>
+</Router> 
+  );
+}
+
+export default App;
---
+++ src/pages/NuestroEscenario.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Home.css';
+
+function NuestroEscenario() {
+return (
+    <div className="Home">
+    <h1>Bienvenido a la página de inicio</h1>
+    </div>
+);
+}
+
+export default NuestroEscenario;    
