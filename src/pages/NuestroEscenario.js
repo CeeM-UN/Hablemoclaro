7eadb427f837914525eab210e0a4b33943f66c6a
CeeM-UN <cepia@unal.edu.co>
2024-05-26 21:06:15 -0500

    Arreglo roots

--- src/pages/NuestroEscenario.js
+++ src/pages/NuestroEscenario.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './Home.css';
+import logo from '../Images/imagenlogo.png'; // Importa la imagen del logo aquí
 
-function NuestroEscenario() {
-return (
+
+function Home() {
+  return (
     <div className="Home">
-    <h1>Bienvenido a la página de inicio</h1>
+      <div className="image-container">
+        <img src={logo} alt="Imagen del blog" className="blog-image" />
+        <div className="centered-text">Hablemos Claro</div>
+      </div>
+      <div className="text-container">Las emociones son los colores que pintan el lienzo de nuestra experiencia humana. Desde la euforia de la felicidad hasta la melancolía de la tristeza, cada emoción teje una narrativa única en el tapiz de nuestras vidas. En este viaje intrincado a través del laberinto de las emociones, exploraremos las profundidades de nuestro mundo interno y descubriremos la riqueza que yace dentro de cada sentimiento.</div> {/* Nuevo elemento */}
     </div>
-);
+  );
 }
 
-export default NuestroEscenario;    
+export default Home;
